1c1caddebbb364f75a9135cabf175f6c3a202991
Mark Tareshawty <tarebyte@gmail.com>
2013-10-09 12:41:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OctoBlog::Application.config.secret_key_base = 'ac58a7a31f1d42d929e7350fac77d8c11f931c1475b06c8e7e145f6a8768ab87a5907d63571d4b180aeb3294dc066aaf2ab381bacd7a0361d28ed79d50a52ea3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OctoBlog::Application.config.session_store :cookie_store, key: '_OctoBlog_session'
