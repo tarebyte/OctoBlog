8105403a7e2a874362526927120d36c40b623654
Mark Nguyen <pnguyen1097@gmail.com>
2013-11-10 23:12:52 -0500

    Automatically serve static pages.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,12 @@ class StaticPagesController < ApplicationController
 
   def home
   end
+
+  def show
+    begin
+      render params[:page].to_sym
+    rescue ActionView::MissingTemplate
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -9,4 +9,15 @@ describe StaticPagesController do
     end
   end
 
+  describe "GET others pages" do
+
+    it "should return 404 if the template does not exist." do
+      expect {
+        get "show", page: "this_page_doesnt_exist"
+      }.to raise_error ActionController::RoutingError
+    end
+
+    # TODO: find a way to write test for when the template exists.
+  end
+
 end
