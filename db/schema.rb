fc9c9e37b5f19fcb6d6c29bbf98a2b2e88906692
Mark Tareshawty <tarebyte@gmail.com>
2013-11-18 10:54:44 -0500

    added role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115160317) do
+ActiveRecord::Schema.define(version: 20131115165846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20131115160317) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.string   "role"
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
