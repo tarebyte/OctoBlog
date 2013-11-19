be1d2cc0a6189d0a199cbf5d2349707ab9536791
Mark Tareshawty <tarebyte@gmail.com>
2013-11-19 11:22:55 -0500

    fixed signout bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 OctoBlog::Application.routes.draw do
   root 'static_pages#home'
 
+  # Needs to be first!
+  match '/auth/:provider/callback', to: 'sessions#create',  via: :get
+  match '/auth/failure',            to: redirect('/'),      via: :get
+  match '/signout',                 to: 'sessions#destroy', via: :get
+
   # match '/pages/:page',             to: 'static_pages#show', via: :get
   match '/members',                 to: 'users#index',      via: :get
 
@@ -9,10 +14,5 @@ OctoBlog::Application.routes.draw do
       resources 'posts', path: ''
     end
   end
-
-  match '/auth/:provider/callback', to: 'sessions#create',  via: :get
-  match '/auth/failure',            to: redirect('/'),      via: :get
-  match '/signout',                 to: 'sessions#destroy', via: :get
-
 end
 
