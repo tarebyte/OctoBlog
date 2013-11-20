8f2cde8b952093fc96a496805c03261aac1fa33d
Mark Nguyen <pnguyen1097@gmail.com>
2013-11-20 14:48:05 -0500

    Add avatar URL.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115165846) do
+ActiveRecord::Schema.define(version: 20131120194226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20131115165846) do
     t.datetime "updated_at"
     t.string   "slug"
     t.string   "role"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
