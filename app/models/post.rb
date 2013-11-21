2c65cc80041f82c8c28fc125f2100ec7608c495e
Mark Nguyen <pnguyen1097@gmail.com>
2013-11-21 13:13:58 -0500

    Use Mongoid::Timestamp

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,11 @@
 class Post
   include Mongoid::Document
   include Mongoid::Slug
+  include Mongoid::Timestamps
   include Tenacity
 
   field :title,   type: String
   field :content, type: String
-
-  field :created_at,   type: DateTime, default: Time.now
-  field :last_updated, type: DateTime, default: Time.now
 
   slug :title, :scope => :repo_id
 
